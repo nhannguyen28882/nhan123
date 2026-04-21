f064c97e7d63f2e6d42550257b96667a9e4b9fde
Thiên Nhân <nguyennhan19096@gmail.com>
2026-04-21 09:25:30 +0700

    Dem tu word

---
+++ Bài toán.py
@@ -0,0 +1,16 @@
+n = int (input("nhap so nguyen duong n:))
+
+
+so_chan = 0
+so_le = 0
+
+while n>0:
+    digit = n % 10
+    if digit % 2 == 0
+        so_chan += 1
+    else:
+        so_le += 1
+    n //=10
+
+print("so luong so chan",so_chan)
+print("so luong so le",so_le)
